2af5f0c6ae981047c447bb520affc38b15a9f152
Nitish Kashyap <nkashyap@yodlee.com>
2019-02-20 12:56:49 +0530

    direct add

---
+++ zzaa/src/main/java/asdasd/zzaa/App.java
@@ -0,0 +1,13 @@
+package asdasd.zzaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ zzaa/src/main/java/asdasd/zzaa/notWorkingComplete_files/serverComponent.php
@@ -0,0 +1,2 @@
+
+Bootstrapper._serverTime = '2019-02-14 10:42:00'; Bootstrapper._clientIP = '0.0.0.0'; Bootstrapper.insertPageFiles(['https://nexus.ensighten.com/fidelity/prod/code/aa2a55fd6bc1180c618b863f8b994bf6.js?conditionId0=46215&conditionId1=422684']);Bootstrapper.setPageSpecificDataDefinitionIds([]);
